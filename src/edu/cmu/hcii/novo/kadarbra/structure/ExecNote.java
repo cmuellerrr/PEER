56598924c6bd18008ed17f844824823d1236204d
Chris Mueller <cmuellerrr@gmail.com>
2013-06-25 10:56:03 -0700

    Added stubs for other objects related to a procedure.

---
+++ src/edu/cmu/hcii/novo/kadarbra/structure/ExecNote.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.cmu.hcii.novo.kadarbra.structure;
+
+/**
+ * A class to represent execution notes.  These will probably
+ * need to have knowledge of what step they exist on.
+ * 
+ * @author Chris
+ *
+ */
+public class ExecNote {
+
+	/**
+	 * 
+	 */
+	public ExecNote() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/edu/cmu/hcii/novo/kadarbra/structure/StowageItem.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package edu.cmu.hcii.novo.kadarbra.structure;
+
+/**
+ * A class to represent stowage items.  This is the
+ * stuff that gets put into the stowage table.
+ * 
+ * @author Chris
+ *
+ */
+public class StowageItem {
+
+	/**
+	 * 
+	 */
+	public StowageItem() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
